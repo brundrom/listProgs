2e326ad0c625cbcbab9ccaf8e3299ddd7f49525f
Gennady Petrov <brundrom@gmail.com>
2021-02-07 20:47:09 +0300

    Added last but one program

---
+++ src/main/scala/lastButOne.scala
@@ -0,0 +1,14 @@
+import scala.util.Random
+
+object lastButOne extends App {
+
+  val digits = List.fill(30)(Random.between(0, 30))
+
+  def lastNum(digitList: List[Int]): Int = {
+    val count = digitList.length - 2
+    digitList(count)
+  }
+
+  println(digits)
+  println(lastNum(digits))
+}
